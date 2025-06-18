6bff020a858cca6beb20f193e7a05a24bb7c1ded
Hamid Raza <hamidraza.engr@gmail.com>
2025-06-18 05:08:27 +0500

    Updates

--- lib/core/app_bindings.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../models/home/home_binding.dart';
-
-class AppBindings extends Bindings {
-  @override
-  void dependencies() {
-    HomeBindings().dependencies();
-  }
-}
